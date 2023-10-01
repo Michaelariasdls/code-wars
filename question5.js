13802461f466523f6fb2e21732c402b603916790
Michaelariasdls <michaelariasdls@gmail.com>
2023-10-01 19:29:38 -0400

    Added last codewars

---
+++ question5.js
@@ -0,0 +1,9 @@
+//DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+Solution
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
