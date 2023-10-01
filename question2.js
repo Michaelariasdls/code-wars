a5dc6db21340061466b3a7341d303727cc3681a9
Michaelariasdls <michaelariasdls@gmail.com>
2023-10-01 17:25:50 -0400

    First Commit of completed codewars challenges

---
+++ question2.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// ------------------------------------------------------------------------
+
+Solution
+
+JavaScript:
+const reverseSeq = n =>  {
+   let result = []
+  
+    for(let i = n; i > 0; i--){
+      result.push(i);
+  }
+  return result
+}
---
+++ question4.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+// --------------------------------------------------------------------------------
+
+Solution
+
+
+JavaScript:
+function opposite(number) { 
+  return -1 * number;
+}
