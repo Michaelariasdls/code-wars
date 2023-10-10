601de34c63ea9f538d6fa0643bb79bc63518c41c
Michaelariasdls <michaelariasdls@gmail.com>
2023-10-10 19:35:12 -0400

    daily codewars

---
+++ question13.js
@@ -0,0 +1,12 @@
+// Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+
+
+Solution
+
+let square = function block(a){
+    return a * a
+  }
