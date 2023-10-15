9209691ca9c7f662a6fb059998ada27c3cbec1b1
Michaelariasdls <michaelariasdls@gmail.com>
2023-10-15 18:57:15 -0400

    daily codewars

---
+++ 8kyu/question18.js
@@ -0,0 +1,34 @@
+Training JS #1: create your first JS function and print "Hello World!
+
+// Task
+// Please refer to two example above and write your first JS function.
+
+// mission 1:
+
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+
+
+
+Solution
+
+
+function helloWorld() {
+    let str = 'Hello World!';
+    console.log(str)
+  }
+
+
+//   After this i shall be completing 2-3 Codewar's daily
