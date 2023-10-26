21011fe8a27ba06986095e4165273a823cc68372
Michaelariasdls <michaelariasdls@gmail.com>
2023-10-25 23:00:05 -0400

    daily codewars

---
+++ 8kyu/question25.js
@@ -0,0 +1,32 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+
+
+
+
+
+Solution
+
+function findNeedle(haystack) {
+    let num = haystack.indexOf("needle");
+     if(num !== -1) {
+   return `found the needle at position ${num}`
+     }else{
+     return "needle not found";
+     }
+   }
+
+
+
+//    Cleaner solution
+// function findNeedle(haystack) {
+//     return "found the needle at position " + haystack.indexOf("needle");
+//   }
---
+++ 8kyu/question26.js
@@ -0,0 +1,22 @@
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+
+Solution
+
+function remove(string){
+    if (string.endsWith("!")) {
+      return string.slice(0,-1)
+      }
+    else{
+      return string;
+      }
+    }
+    
