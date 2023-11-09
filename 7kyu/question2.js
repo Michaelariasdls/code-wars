6a2935b59bcab2d23c46788c10ba22b495d5dd3b
Michaelariasdls <michaelariasdls@gmail.com>
2023-11-09 18:51:10 -0500

    codewars

---
+++ 7kyu/question2.js
@@ -0,0 +1,20 @@
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+  
+    let reversedString = str.match(/[a-zA-Z]/g).reverse().join('');
+  
+    return reversedString;
+  }
