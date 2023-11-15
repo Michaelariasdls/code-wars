0e0d5b2975a41b7497d385bbda714e480362e204
Michaelariasdls <michaelariasdls@gmail.com>
2023-11-14 21:24:08 -0500

    daily codewars

---
+++ 8kyu/question38.js
@@ -0,0 +1,14 @@
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+Solution
+
+function maps(x){
+    return x.map(x => 2 * x);
+      
+    }
